d967cf4e27e165cdd0510d1c1c461fb2de91dfc4
n-peugnet <n.peugnet@free.fr>
2020-02-17 00:06:48 +0100

    autoformat TME2 because of the horrible 3 space indent

--- TME2_philo.c
+++ TME2_philo.c
@@ -40,119 +40,121 @@ int right_chopstick = 0;	// je n'ai pas celle de droite
 //************   LES REPAS 
 int meals_eaten = 0;		// nb de repas consommes localement
 
-
-//************   LES FONCTIONS   *************************** 
-int max(int a, int b) {
-   return (a>b?a:b);
+//************   LES FONCTIONS   ***************************
+int max(int a, int b)
+{
+	return (a > b ? a : b);
 }
 
-void receive_message(MPI_Status *status) {
-   MPI_Recv(NULL, 0, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, status);
+void receive_message(MPI_Status *status)
+{
+	MPI_Recv(NULL, 0, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, status);
 }
 
-void send_message(int dest, int tag) {
-   printf("P%d> Send message %d to %d\n", rank, tag, dest);
-   MPI_Send(NULL, 0, MPI_INT, dest, tag, MPI_COMM_WORLD);
+void send_message(int dest, int tag)
+{
+	printf("P%d> Send message %d to %d\n", rank, tag, dest);
+	MPI_Send(NULL, 0, MPI_INT, dest, tag, MPI_COMM_WORLD);
 }
 
 /* renvoie 0 si le noeud local et ses 2 voisins sont termines */
-int check_termination() {
-   if (left_state == DONE && right_state == DONE && meals_eaten == NB_MEALS){
-      return 0;
-   }
-   return 1;
+int check_termination()
+{
+	if (left_state == DONE && right_state == DONE && meals_eaten == NB_MEALS) {
+		return 0;
+	}
+	return 1;
 }
 
-void eat() {
-   printf("P%d> Eating\n", rank);
-   sleep(1);
-   if (++meals_eaten == NB_MEALS) {
-      local_state = DONE;
-      send_message(left, DONE_EATING);
-      send_message(right, DONE_EATING);
-   }
-   left_chopstick = 0;		// je n'ai pas celle de gauche
-   right_chopstick = 0;	   // je n'ai pas celle de droite
-
+void eat()
+{
+	printf("P%d> Eating\n", rank);
+	sleep(1);
+	if (++meals_eaten == NB_MEALS) {
+		local_state = DONE;
+		send_message(left, DONE_EATING);
+		send_message(right, DONE_EATING);
+	}
+	left_chopstick = 0; // je n'ai pas celle de gauche
+	right_chopstick = 0; // je n'ai pas celle de droite
 }
 
-void change_state() {
-   if (local_state == HUNGRY) {
-      printf("P%d> Thinking\n", rank);
-      local_state = THINKING;
-      left_chopstick = 0;
-      right_chopstick = 0;
-   } else {
-      printf("P%d> Waiting\n", rank);
-      local_state = HUNGRY;
-   }
+void change_state()
+{
+	if (local_state == HUNGRY) {
+		printf("P%d> Thinking\n", rank);
+		local_state = THINKING;
+		left_chopstick = 0;
+		right_chopstick = 0;
+	} else {
+		printf("P%d> Waiting\n", rank);
+		local_state = HUNGRY;
+	}
 }
 
-//************   LE PROGRAMME   *************************** 
-int main(int argc, char* argv[]) {
+//************   LE PROGRAMME   ***************************
+int main(int argc, char *argv[])
+{
 
-   MPI_Status status;
+	MPI_Status status;
 
-   MPI_Init(&argc, &argv);
-   MPI_Comm_size(MPI_COMM_WORLD, &NB);
-   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	
-   left = (rank + NB - 1) % NB;
-   right = (rank + 1) % NB;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &NB);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-   while(check_termination()) {
+	left = (rank + NB - 1) % NB;
+	right = (rank + 1) % NB;
 
-      /* Tant qu'on n'a pas fini tous ses repas, redemander les 2 baguettes  
-         a chaque fin de repas */
-      if ((meals_eaten < NB_MEALS) && (local_state == HUNGRY)) {
-         //demande de baguette aux 2 voisins
-         send_message(right, WANNA_CHOPSTICK);
-         send_message(left, WANNA_CHOPSTICK);
-         printf("P%d> Asking for chopsticks at %d\n", rank, local_clock);
-         
-      }
-		
-      receive_message(&status);
-		
-      if (status.MPI_TAG == DONE_EATING) {
-         //Enregistrer qu'un voisin a fini de manger
-         if (left == status.MPI_SOURCE) {
-            left_state = DONE;
-         } else if (right == status.MPI_SOURCE) {
-            right_state = DONE;
-         }
-	     
-      } else if (status.MPI_TAG == CHOPSTICK_YOURS) {
-         if (local_state == HUNGRY) {
-            if (left == status.MPI_SOURCE) {
-               left_chopstick = 1;
-            } else if (right == status.MPI_SOURCE) {
-               right_chopstick = 1;
-            }
-            if (left_chopstick == 1 && right_chopstick == 1) {
-               eat();
-            }
-         }
-      } else {
-         /* c'est une demande */
-         if (local_state != HUNGRY) {
-            send_message(status.MPI_SOURCE, CHOPSTICK_YOURS);
-         } else if (rank > 0 && rank > status.MPI_SOURCE) {
-            send_message(status.MPI_SOURCE, CHOPSTICK_YOURS);
-            if (left == status.MPI_SOURCE)
-               left_chopstick = 0;
-            else if (right == status.MPI_SOURCE)
-               right_chopstick = 0;
-         }
+	while (check_termination()) {
 
-      }
+		/* Tant qu'on n'a pas fini tous ses repas, redemander les 2 baguette
+		   a chaque fin de repas */
+		if ((meals_eaten < NB_MEALS) && (local_state == HUNGRY)) {
+			//demande de baguette aux 2 voisins
+			send_message(right, WANNA_CHOPSTICK);
+			send_message(left, WANNA_CHOPSTICK);
+			printf("P%d> Asking for chopsticks at %d\n", rank, local_clock);
+		}
 
-      // Change between HUNGRY and THINKING
-      local_clock++;
-      if (local_clock % DURATION == 0 )
-         change_state();
-   }
-	
-   MPI_Finalize();
-   return 0;
+		receive_message(&status);
+
+		if (status.MPI_TAG == DONE_EATING) {
+			//Enregistrer qu'un voisin a fini de manger
+			if (left == status.MPI_SOURCE) {
+				left_state = DONE;
+			} else if (right == status.MPI_SOURCE) {
+				right_state = DONE;
+			}
+		} else if (status.MPI_TAG == CHOPSTICK_YOURS) {
+			if (local_state == HUNGRY) {
+				if (left == status.MPI_SOURCE) {
+					left_chopstick = 1;
+				} else if (right == status.MPI_SOURCE) {
+					right_chopstick = 1;
+				}
+				if (left_chopstick == 1 && right_chopstick == 1) {
+					eat();
+				}
+			}
+		} else {
+			/* c'est une demande */
+			if (local_state != HUNGRY) {
+				send_message(status.MPI_SOURCE, CHOPSTICK_YOURS);
+			} else if (rank > 0 && rank > status.MPI_SOURCE) {
+				send_message(status.MPI_SOURCE, CHOPSTICK_YOURS);
+				if (left == status.MPI_SOURCE)
+					left_chopstick = 0;
+				else if (right == status.MPI_SOURCE)
+					right_chopstick = 0;
+			}
+		}
+
+		// Change between HUNGRY and THINKING
+		local_clock++;
+		if (local_clock % DURATION == 0)
+			change_state();
+	}
+
+	MPI_Finalize();
+	return 0;
 }
